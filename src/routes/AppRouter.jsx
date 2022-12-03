375ff4a755c17ef2b203b5df1bd3c005f5e59286
APEREZB26 <aperez26@autonoma.edu.pe>
2022-12-03 10:46:01 -0500

    actualización

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,19 +5,23 @@ import { Homepage } from "../views/Homepage";
 import { ListProduct } from "../views/ListProduct";
 import { ListClient } from "../views/ListClient";
 import { Product } from "../views/Product";
+import { Message } from "../components/ui/Message";
 
 export function AppRouter() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/homepage" element={<Homepage/>}/>
-        <Route path="/product" element={<Product/>}/>
-        <Route path="/admin/listProduct" element={<ListProduct/>}/>
-        <Route path="/admin/listClient" element={<ListClient/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Message/>}/>
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/admin/listProduct" element={<ListProduct />} />
+          <Route path="/admin/listClient" element={<ListClient />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
