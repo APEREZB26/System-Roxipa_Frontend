9dd5ecc5888fa244b03b400e983e9d4ea2a83642
Jorge Ore <jore24@autonoma.edu.pe>
2022-12-08 13:38:13 -0500

    update server chat

--- src/components/ui/chat.jsx
+++ src/components/ui/chat.jsx
@@ -1,11 +1,40 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
-import {faQuestionCircle} from "@fortawesome/free-regular-svg-icons";
-import { useState } from "react";
+import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
 import "../../styles/chat.css";
 
+const socket = io("/");
+
 export const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+    };
+
+    socket.on("message", receiveMessage);
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, [messages]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+    setMessage("");
+    socket.emit("message", newMessage.body);
+  };
+
   const [addClass, setaddClass] = useState(false);
 
   const activeButton = () => {
@@ -16,34 +45,32 @@ export const Chat = () => {
   };
 
   return (
-    <div>
-      <button className="chatButton" onClick={activeButton}>
-        <FontAwesomeIcon icon={faQuestionCircle} />
-      </button>
-      <div className={`chatContainer${addClass ? " active" : ""}`}>
-        <div className="chatHeader">
-          <button className="exit" onClick={noActiveButton}>
-            <FontAwesomeIcon icon={faCircleArrowLeft} />
-          </button>
-          <p>Atención al Cliente</p>
-        </div>
-        <div className="chatBody">
-          <li>Mensaje N°1</li>
-          <li>Mensaje N°2</li>
-          <li>Mensaje N°3</li>
-          <li>Mensaje N°4</li>
-          <li>Mensaje N°5</li>
-          <li>Mensaje N°1</li>
-          <li>Mensaje N°2</li>
-          <li>Mensaje N°3</li>
-          <li>Mensaje N°4</li>
-          <li>Mensaje N°5</li>
-        </div>
-        <div className="chatFooter">
-          <input type="text"></input>
-          <FontAwesomeIcon className="send" icon={faPaperPlane} />
-        </div>
-      </div>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+              }`}
+            >
+              <b>{message.from}</b>:{message.body}
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 };
